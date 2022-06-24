7834d98a124af6c204ff72126d7254d17c2bc581
Robert Bel <rbel@brandsafway.com>
2022-06-23 19:00:11 -0500

    Uploaded by Quasar Publish Script

--- assets/axios.1945e043.js
+++
@@ -1,1 +0,0 @@
-import{f as a,k as s,s as e}from"./index.2b12213f.js";const i=e(s.create({timeout:25e3}),{maxRequests:6,perMilliseconds:1e3});var t=a(({app:o})=>{o.config.globalProperties.$axios=s,o.config.globalProperties.$api=i});export{i as api,t as default};
---
+++ assets/pinia.46dbd75c.js
@@ -0,0 +1,1 @@
+import{f as e,j as i}from"./index.5b45782f.js";var r=e(({app:a})=>{a.use(i())});export{r as default};
